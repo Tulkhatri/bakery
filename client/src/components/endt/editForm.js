ff195ca70414c714ff0bec124d3663039909b1bd
Tulkhatri <tulkhatri01@gmail.com>
2023-01-19 12:32:05 +0545

    features: custom dialog box is open  when product is clicked

--- client/src/components/endt/editForm.js
+++ client/src/components/endt/editForm.js
@@ -1,6 +1,6 @@
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import {Modal } from 'antd';
+import { Modal } from 'antd';
 import { useState } from 'react';
 const usersSchema = Yup.object().shape({
 });
--- client/src/containers/sharedScreens/products.js
+++ client/src/containers/sharedScreens/products.js
@@ -4,7 +4,8 @@ import Card from '../../components/content/card';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from "axios";
-const Products = () => {
+import { Modal } from 'antd';
+const Products = (props) => {
     const [validItems, setValidItems] = useState([])
     const fetchProducts = () => {
         axios.get("http://localhost:3005/products").then((res) => {
@@ -15,19 +16,31 @@ const Products = () => {
     useEffect(() => {
         fetchProducts()
     }, [])
-
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const showModal = () => {
+      setIsModalOpen(true);
+      props.addProduct()
+    };
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
     return (
         <>
             <div className='admin_drawer'>
                 <Link to='/'> <FontAwesomeIcon icon={faBars} className="admin_icon"></FontAwesomeIcon> </Link>
             </div>
-            <div className="card_main_div"onClick={(()=>alert("hello"))}>
+            <Modal title="  " open={isModalOpen} onOk={handleOk} onCancel={handleCancel}  footer={null}> </Modal>
+            <div className="card_main_div"onClick={(()=> setIsModalOpen(true))}>
                 {validItems.map((items) => {
                     return <Card items={items} />
                 }
-
+                
                 )}
             </div>
+               
         </>
     );
 
