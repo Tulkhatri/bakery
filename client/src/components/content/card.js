4609852c512a9a0f583c9f8b4e2c56c64ec7475e
Tulkhatri <tulkhatri01@gmail.com>
2023-01-19 13:14:35 +0545

    features fix: custom dialog box is open  when product is clicked

--- client/src/components/content/card.js
+++ client/src/components/content/card.js
@@ -1,11 +1,20 @@
+import React, { useState } from 'react'
+import { Modal } from 'antd';
 const Card = (props) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
     return (
-        <>
-                <div className="card_view">
-                    <div className="card_image">{props.items.image}</div>
-                    <div className="card_name">{props.items.name}</div>
-                    <div className="card_price">{props.items.price}</div>
-                </div>
+        <> <Modal title="  " open={isModalOpen} onOk={handleOk} onCancel={handleCancel} footer={null}> </Modal>
+            <div className="card_view" onClick={() => setIsModalOpen(true)}>
+                <div className="card_image">{props.items.image}</div>
+                <div className="card_name">{props.items.name}</div>
+                <div className="card_price">{props.items.price}</div>
+            </div>
         </>
     );
 }
--- client/src/containers/sharedScreens/products.js
+++ client/src/containers/sharedScreens/products.js
@@ -4,45 +4,30 @@ import Card from '../../components/content/card';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from "axios";
-import { Modal } from 'antd';
-const Products = (props) => {
+const Products = () => {
     const [validItems, setValidItems] = useState([])
     const fetchProducts = () => {
         axios.get("http://localhost:3005/products").then((res) => {
             setValidItems(res.data.products)
         })
     }
-
     useEffect(() => {
         fetchProducts()
     }, [])
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const showModal = () => {
-      setIsModalOpen(true);
-      props.addProduct()
-    };
-    const handleOk = () => {
-      setIsModalOpen(false);
-    };
-    const handleCancel = () => {
-      setIsModalOpen(false);
-    };
+
     return (
         <>
             <div className='admin_drawer'>
                 <Link to='/'> <FontAwesomeIcon icon={faBars} className="admin_icon"></FontAwesomeIcon> </Link>
             </div>
-            <Modal title="  " open={isModalOpen} onOk={handleOk} onCancel={handleCancel}  footer={null}> </Modal>
-            <div className="card_main_div"onClick={(()=> setIsModalOpen(true))}>
+            <div className="card_main_div">
                 {validItems.map((items) => {
                     return <Card items={items} />
                 }
-                
+
                 )}
             </div>
-               
         </>
     );
-
 }
 export default Products;
